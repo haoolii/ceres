488963d3639ddf7cf624816f7841c46d4a209a6d
unnhao <unnhao@gmail.com>
2023-10-31 11:12:10 +0800

    TEST IP

--- app/my-ip/page.tsx
+++ app/my-ip/page.tsx
@@ -1,7 +1,9 @@
+import { headers } from "next/headers";
 export default function MyIP() {
+    const forwardedFor = headers().get("x-forwarded-for");
     return (
         <div>
-            My IP
+            My IP {forwardedFor}
         </div>
     )
 }
