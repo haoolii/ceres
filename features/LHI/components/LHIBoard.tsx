78b6c65f2690fa1f89a43689487676026403bc9f
Hao <unnhao@gmail.com>
2023-11-01 02:27:13 +0800

    feat: add 勞退 健保

--- features/LHI/components/LHIBoard.tsx
+++ features/LHI/components/LHIBoard.tsx
@@ -1,27 +1,31 @@
 "use client"
 
-import { useEffect, useMemo, useState } from "react"
+import { useMemo, useState } from "react"
 import { Input } from "@/components/ui/input";
-import { calculate勞保 } from "../utils/calculate";
+import { calculate健保, calculate勞保, calculate勞退 } from "../utils/calculate";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
 
 export const LHIBoard = () => {
-    const [salary, setSalary] = useState<number>(0)
+    const [salary, setSalary] = useState<number>(0);
+    const [identity, setIdentity] = useState<"本人" | "本人+1" | "本人+2" | "本人+3">("本人");
     const handleSalaryChange = (input: string) => {
         setSalary(+input.toString().replace(/[^-.0-9]/g, ''));
     }
     const info = useMemo(() => {
         if (!salary) {
             return {
-                勞保: null
+                勞保: null,
+                健保: null,
+                勞退: null
             }
         }
         return {
-            勞保: calculate勞保(salary)
+            勞保: calculate勞保(salary),
+            健保: calculate健保(salary),
+            勞退: calculate勞退(salary),
         };
     }, [salary]);
     return (
@@ -37,20 +41,20 @@ export const LHIBoard = () => {
                     <CardContent className="grid gap-4">
                         <div className="grid gap-2">
                             <Label htmlFor="salary">薪資</Label>
-                            <Input id="salary" placeholder="薪資" value={salary || ""} onChange={(e) => handleSalaryChange(e.target.value)} />
+                            <Input id="salary" type="number" placeholder="薪資" value={salary || ""} onChange={(e) => handleSalaryChange(e.target.value)} />
                         </div>
                         <div className="grid gap-2">
                             <Label htmlFor="who">扶養</Label>
-                            <Select name="who">
+                            <Select value={identity} onValueChange={(v: "本人" | "本人+1" | "本人+2" | "本人+3") => setIdentity(v)}>
                                 <SelectTrigger>
-                                    <SelectValue placeholder="扶養" />
+                                    <SelectValue placeholder="" />
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectGroup>
-                                        <SelectLabel>本人</SelectLabel>
-                                        <SelectItem value="+1">本人與一位眷屬</SelectItem>
-                                        <SelectItem value="+2">本人與二位眷屬</SelectItem>
-                                        <SelectItem value="+3">本人與三位眷屬</SelectItem>
+                                        <SelectItem value="本人">本人</SelectItem>
+                                        <SelectItem value="本人+1">本人與一位眷屬</SelectItem>
+                                        <SelectItem value="本人+2">本人與二位眷屬</SelectItem>
+                                        <SelectItem value="本人+3">本人與三位眷屬</SelectItem>
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
@@ -91,16 +95,16 @@ export const LHIBoard = () => {
                                 <div className=" space-y-1 text-left flex-1">
                                     <p className=" text-md font-medium leading-none">健保費</p>
                                 </div>
-                                <div className="ml-auto font-medium text-right flex-1">{0}</div>
-                                <div className="ml-auto font-medium text-right flex-1">{0}</div>
+                                <div className="ml-auto font-medium text-right flex-1">{info?.健保 && info?.健保[identity] || 0}</div>
+                                <div className="ml-auto font-medium text-right flex-1">{info?.健保?.投保單位負擔金額 || 0}</div>
                             </div>
                             <Separator />
                             <div className="flex items-center">
                                 <div className=" space-y-1 text-left flex-1">
                                     <p className=" text-md font-medium leading-none">員工退休金</p>
                                 </div>
-                                <div className="ml-auto font-medium text-right flex-1">{0}</div>
-                                <div className="ml-auto font-medium text-right flex-1">{0}</div>
+                                <div className="ml-auto font-medium text-right flex-1"></div>
+                                <div className="ml-auto font-medium text-right flex-1">{info?.勞退?.提繳金額 || 0}</div>
                             </div>
                             <Separator />
                             <div className="flex items-center">
@@ -113,7 +117,7 @@ export const LHIBoard = () => {
                         </div>
                         {/* <Separator className="my-4" /> */}
                         <div>
-                            
+
                         </div>
                     </CardContent>
                     <CardFooter>
--- features/LHI/utils/calculate.ts
+++ features/LHI/utils/calculate.ts
@@ -1,13 +1,21 @@
 import { useMemo, useState } from "react";
-import { 勞保資料 } from "../constants";
+import { 勞保資料, 健保資料, 勞退資料 } from "../constants";
 
-const levels = Object.keys(勞保資料)
+export const 勞保級距 = Object.keys(勞保資料)
+  .map((v) => +v)
+  .sort((a, b) => a - b);
+
+export const 健保級距 = Object.keys(健保資料)
+  .map((v) => +v)
+  .sort((a, b) => a - b);
+
+export const 勞退級距 = Object.keys(勞退資料)
   .map((v) => +v)
   .sort((a, b) => a - b);
   
 export const calculate勞保 = (salary: number) => {
-  let targetLevel = levels[levels.length - 1];
-  for (let level of levels) {
+  let targetLevel = 勞保級距[勞保級距.length - 1];
+  for (let level of 勞保級距) {
     if (level >= salary && level < targetLevel) {
       targetLevel = level;
     }
@@ -15,6 +23,22 @@ export const calculate勞保 = (salary: number) => {
   return 勞保資料[`${targetLevel}` as keyof typeof 勞保資料];
 };
 
-export const use健保 = () => {};
+export const calculate健保 = (salary: number) => {
+  let targetLevel = 健保級距[健保級距.length - 1];
+  for (let level of 健保級距) {
+    if (level >= salary && level < targetLevel) {
+      targetLevel = level;
+    }
+  }
+  return 健保資料[`${targetLevel}` as keyof typeof 健保資料];
+};
 
-export const use勞退 = () => {};
+export const calculate勞退 = (salary: number) => {
+  let targetLevel = 勞退級距[勞退級距.length - 1];
+  for (let level of 勞退級距) {
+    if (level >= salary && level < targetLevel) {
+      targetLevel = level;
+    }
+  }
+  return 勞退資料[`${targetLevel}` as keyof typeof 勞退資料];
+};
