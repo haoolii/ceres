9d9e50649db337c28f2bdd3b7227a8f233fe7a1c
unnhao <unnhao@gmail.com>
2023-11-03 23:46:49 +0800

    feat: add meta data

--- app/image-color-picker/page.tsx
+++ app/image-color-picker/page.tsx
@@ -1,5 +1,21 @@
 import { ImageColorPickerBoard } from "@/features/image-color-picker/components/imageColorPickerBoard";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "圖片色碼選取",
+  description:
+    "圖像色彩工具，可以幫助我們選取圖片中的正確的顏色色碼，支援HEX色碼與RGB色碼。",
+  keywords: [
+    "圖像",
+    "色彩",
+    "色碼",
+    "選取",
+    "選取器",
+    "圖像色碼選取",
+    "色碼選取",
+    "RGB",
+    "HEX",
+  ],
+};
 export default function ImageColorPicker() {
   return (
     <div className="container py-6">
